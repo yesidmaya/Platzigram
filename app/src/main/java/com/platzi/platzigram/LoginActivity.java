9d1af53bee5e14da81a7cc67e711685d81615ccc
alejandro <yesidmaya@gmail.com>
2018-04-05 11:47:59 -0500

    se agrego el bottombar

--- app/src/main/java/com/platzi/platzigram/LoginActivity.java
+++ app/src/main/java/com/platzi/platzigram/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.platzi.platzigram.view.ContainerActivity;
 import com.platzi.platzigram.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -17,6 +18,11 @@ public class LoginActivity extends AppCompatActivity {
 
     public void goCreateAccount(View view){
         Intent intent = new Intent(this, CreateAccountActivity.class); // el primer parametro significa en que clase estoy ubicado // el segundo parametro significa a donde quiero ir
+        startActivity(intent);
+    }
+
+    public void goLogin(View view){
+        Intent intent = new Intent(this, ContainerActivity.class);
         startActivity(intent);
     }
 }
